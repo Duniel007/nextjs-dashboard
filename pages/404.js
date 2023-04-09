30db74a211c5ff32d0323587dc238be6e8350e93
Duniel007 <Greenpl243@gmail.com>
2023-04-09 02:01:28 +0200

    deploy

---
+++ pages/404.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const fourzerofour = () => {
+  return (
+    <main className='bg-gray-100 min-h-screen'>
+        404
+    </main>
+  )
+}
+
+export default fourzerofour
